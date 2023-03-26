f3ad387d5e3efad7c90e7a9b888aae71967e8d5c
sital999 <sitalnagarkoti123@gmail.com>
2023-03-26 10:26:15 +0545

    use dropdown on add-book section

--- frontend/src/component/AddBook.jsx
+++ frontend/src/component/AddBook.jsx
@@ -1,14 +1,14 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import { useAddBookMutation } from "../features/book/bookApi";
 import { Departments } from "../constants/department";
 import { Genres } from "../constants/genre";
+import {Semesters} from "../constants/semester";
 import {Dropdown } from "../elements"
 
 const AddBook = () => {
       const [addBook] = useAddBookMutation()
       const [book,setBook]=useState({
         name:"",
-        genre:"",
         rent_price:"",
         selling_price:"",
         semester:"",
@@ -16,6 +16,13 @@ const AddBook = () => {
         description:"",
       })
       const [bookImage,setBookImage]=useState("")
+
+      // for dropdown
+      const [genre,setGenre]=useState("")
+      const [department, setDepartment]=useState("");
+      const [semester,setSemester]=useState("") ;
+      const [clicked,setClicked]=useState(false)
+
 
       const handleChange=(e)=>{
         const name=e.target.name
@@ -26,13 +33,14 @@ const AddBook = () => {
 
       const handleClick=()=>{
         formdata.append('name',book.name)
-        formdata.append("genre", book.genre);
+        formdata.append("genre", genre);
         formdata.append("rent_price", book.rent_price);
         formdata.append("selling_price", book.selling_price);
-        formdata.append("semester", book.semester);
-        formdata.append("department", book.department);
+        formdata.append("semester", semester);
+        formdata.append("department", department);
         formdata.append("description", book.description);
         formdata.append('bookImage',bookImage)
+        setClicked(true)
         addBook(formdata).then((datas)=>{
             console.log(datas)
         }).catch((err)=>{console.log(err)})
@@ -46,11 +54,15 @@ const AddBook = () => {
           description: "",
         });
       }
+      useEffect(()=>{
+        setClicked(false)
+      },[genre,department,semester])
+
 
   return (
     <div className="mb-10 flex flex-col place-items-center gap-4">
-        <h1 className="font-bold text-2xl">ADD BOOK</h1>
-      <div className=" rounded-3xl ring-2 ring-slate-50 bg-box-bg-color h-2/5 w-5/12 p-4 space-y-4 py-8">
+      <h1 className="font-bold text-2xl">ADD BOOK</h1>
+      <div className=" rounded-3xl ring-2 ring-slate-50 bg-box-bg-color h-2/5 w-9/12 p-4 space-y-4 py-8">
         <div className="flex flex-col gap-8 items-center">
           <div className="flex">
             <div className="basis-1/2 text-start pl-4 space-y-6 font-semibold text-lg">
@@ -71,13 +83,19 @@ const AddBook = () => {
                 onChange={handleChange}
                 className="bg-inputBox p-1 px-3 rounded-md ring-2 w-7/12 ring-slate-50"
               />
-              <input
+              <Dropdown
+                title={"Genre"}
+                contents={Genres}
+                setValue={setGenre}
+                clicked={clicked}
+              />
+              {/* <input
                 type="text"
                 name="genre"
                 value={book.genre}
                 onChange={handleChange}
                 className="bg-inputBox p-1 px-3 rounded-md ring-2 w-7/12 ring-slate-50"
-              />
+              /> */}
               <input
                 type="number"
                 name="rent_price"
@@ -92,20 +110,32 @@ const AddBook = () => {
                 onChange={handleChange}
                 className="bg-inputBox p-1 px-3 rounded-md ring-2 w-7/12 ring-slate-50"
               />
-              <input
+              <Dropdown
+                title={"Semester"}
+                contents={Semesters}
+                setValue={setSemester}
+                clicked={clicked}
+              />
+              {/* <input
                 type="text"
                 name="semester"
                 value={book.semester}
                 onChange={handleChange}
                 className="bg-inputBox p-1 px-3 rounded-md ring-2 w-7/12 ring-slate-50"
+              /> */}
+              <Dropdown
+                title={"Department"}
+                contents={Departments}
+                setValue={setDepartment}
+                clicked={clicked}
               />
-              <input
+              {/* <input
                 type="text"
                 name="department"
                 value={book.department}
                 onChange={handleChange}
                 className="bg-inputBox p-1 px-3 rounded-md ring-2 w-7/12 ring-slate-50"
-              />
+              /> */}
               <input
                 type="file"
                 defaultValue={bookImage}
@@ -121,7 +151,12 @@ const AddBook = () => {
               />
             </div>
           </div>
-          <button className="bg-buyButton p-2 w-28 rounded-full" onClick={handleClick}>Add Book</button>
+          <button
+            className="bg-buyButton p-2 w-28 rounded-full"
+            onClick={handleClick}
+          >
+            Add Book
+          </button>
         </div>
       </div>
     </div>
